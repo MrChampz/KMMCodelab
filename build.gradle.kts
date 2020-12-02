281376e0259b6c931aa6609647dc71cad629cd7d
Felipe Vieira <mrchampz@outlook.com>
2020-12-02 00:35:44 -0300

    Entities model classes

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+buildscript {
+    val kotlinVersion = "1.4.10"
+    val sqlDelightVersion: String by project
+
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+        classpath("com.android.tools.build:gradle:4.0.2")
+        classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        mavenCentral()
+    }
+}
